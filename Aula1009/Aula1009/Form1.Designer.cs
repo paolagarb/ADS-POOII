7c70b737a5a3027c986c0307660f91ee2757d62d
Paola Garbato <paolagarbato@gmail.com>
2020-09-15 08:10:27 -0300

    Exercício: gerar um relatóio com as informações de um formulário - finalizado.

---
+++ Aula1009/Aula1009/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Aula1009
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblIdade = new System.Windows.Forms.Label();
+            this.lblEndereco = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.lblCadastro = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.txtEndereço = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.btnGerarRelatorio = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblRelatorioNome = new System.Windows.Forms.Label();
+            this.lblRelatorioIdade = new System.Windows.Forms.Label();
+            this.lblRelatorioEndereco = new System.Windows.Forms.Label();
+            this.lblRelatorioEmail = new System.Windows.Forms.Label();
+            this.lblRelatorioTelefone = new System.Windows.Forms.Label();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.ForeColor = System.Drawing.Color.White;
+            this.lblNome.Location = new System.Drawing.Point(77, 84);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(51, 20);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblIdade
+            // 
+            this.lblIdade.AutoSize = true;
+            this.lblIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdade.ForeColor = System.Drawing.Color.White;
+            this.lblIdade.Location = new System.Drawing.Point(80, 125);
+            this.lblIdade.Name = "lblIdade";
+            this.lblIdade.Size = new System.Drawing.Size(50, 20);
+            this.lblIdade.TabIndex = 1;
+            this.lblIdade.Text = "Idade";
+            // 
+            // lblEndereco
+            // 
+            this.lblEndereco.AutoSize = true;
+            this.lblEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEndereco.ForeColor = System.Drawing.Color.White;
+            this.lblEndereco.Location = new System.Drawing.Point(80, 171);
+            this.lblEndereco.Name = "lblEndereco";
+            this.lblEndereco.Size = new System.Drawing.Size(78, 20);
+            this.lblEndereco.TabIndex = 2;
+            this.lblEndereco.Text = "Endereço";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.Color.White;
+            this.lblEmail.Location = new System.Drawing.Point(80, 217);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(48, 20);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefone.ForeColor = System.Drawing.Color.White;
+            this.lblTelefone.Location = new System.Drawing.Point(80, 266);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(71, 20);
+            this.lblTelefone.TabIndex = 4;
+            this.lblTelefone.Text = "Telefone";
+            // 
+            // lblCadastro
+            // 
+            this.lblCadastro.AutoSize = true;
+            this.lblCadastro.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCadastro.Font = new System.Drawing.Font("Microsoft Uighur", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCadastro.ForeColor = System.Drawing.Color.White;
+            this.lblCadastro.Location = new System.Drawing.Point(204, 24);
+            this.lblCadastro.Name = "lblCadastro";
+            this.lblCadastro.Size = new System.Drawing.Size(109, 40);
+            this.lblCadastro.TabIndex = 5;
+            this.lblCadastro.Text = "Relatório";
+            // 
+            // txtNome
+            // 
+            this.txtNome.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(174, 83);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(227, 22);
+            this.txtNome.TabIndex = 6;
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIdade.Location = new System.Drawing.Point(174, 126);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(227, 22);
+            this.txtIdade.TabIndex = 7;
+            this.txtIdade.TextChanged += new System.EventHandler(this.txtIdade_TextChanged);
+            // 
+            // txtEndereço
+            // 
+            this.txtEndereço.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEndereço.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEndereço.Location = new System.Drawing.Point(174, 172);
+            this.txtEndereço.Name = "txtEndereço";
+            this.txtEndereço.Size = new System.Drawing.Size(227, 22);
+            this.txtEndereço.TabIndex = 8;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(174, 218);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(227, 22);
+            this.txtEmail.TabIndex = 9;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTelefone.Location = new System.Drawing.Point(174, 267);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(227, 22);
+            this.txtTelefone.TabIndex = 10;
+            // 
+            // btnGerarRelatorio
+            // 
+            this.btnGerarRelatorio.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGerarRelatorio.FlatAppearance.BorderSize = 0;
+            this.btnGerarRelatorio.FlatAppearance.MouseDownBackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnGerarRelatorio.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Snow;
+            this.btnGerarRelatorio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGerarRelatorio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGerarRelatorio.ForeColor = System.Drawing.Color.Black;
+            this.btnGerarRelatorio.Location = new System.Drawing.Point(196, 319);
+            this.btnGerarRelatorio.Name = "btnGerarRelatorio";
+            this.btnGerarRelatorio.Size = new System.Drawing.Size(122, 28);
+            this.btnGerarRelatorio.TabIndex = 11;
+            this.btnGerarRelatorio.Text = "Gerar Relatório";
+            this.btnGerarRelatorio.UseVisualStyleBackColor = false;
+            this.btnGerarRelatorio.Click += new System.EventHandler(this.botApresentar);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.Controls.Add(this.btnConfirmar);
+            this.panel1.Controls.Add(this.lblRelatorioTelefone);
+            this.panel1.Controls.Add(this.lblRelatorioEmail);
+            this.panel1.Controls.Add(this.lblRelatorioEndereco);
+            this.panel1.Controls.Add(this.lblRelatorioIdade);
+            this.panel1.Controls.Add(this.lblRelatorioNome);
+            this.panel1.Location = new System.Drawing.Point(69, 84);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(367, 264);
+            this.panel1.TabIndex = 12;
+            this.panel1.Visible = false;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lblRelatorioNome
+            // 
+            this.lblRelatorioNome.AutoSize = true;
+            this.lblRelatorioNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRelatorioNome.ForeColor = System.Drawing.Color.Black;
+            this.lblRelatorioNome.Location = new System.Drawing.Point(23, 19);
+            this.lblRelatorioNome.Name = "lblRelatorioNome";
+            this.lblRelatorioNome.Size = new System.Drawing.Size(51, 20);
+            this.lblRelatorioNome.TabIndex = 1;
+            this.lblRelatorioNome.Text = "Nome";
+            // 
+            // lblRelatorioIdade
+            // 
+            this.lblRelatorioIdade.AutoSize = true;
+            this.lblRelatorioIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRelatorioIdade.ForeColor = System.Drawing.Color.Black;
+            this.lblRelatorioIdade.Location = new System.Drawing.Point(24, 57);
+            this.lblRelatorioIdade.Name = "lblRelatorioIdade";
+            this.lblRelatorioIdade.Size = new System.Drawing.Size(50, 20);
+            this.lblRelatorioIdade.TabIndex = 2;
+            this.lblRelatorioIdade.Text = "Idade";
+            // 
+            // lblRelatorioEndereco
+            // 
+            this.lblRelatorioEndereco.AutoSize = true;
+            this.lblRelatorioEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRelatorioEndereco.ForeColor = System.Drawing.Color.Black;
+            this.lblRelatorioEndereco.Location = new System.Drawing.Point(24, 99);
+            this.lblRelatorioEndereco.Name = "lblRelatorioEndereco";
+            this.lblRelatorioEndereco.Size = new System.Drawing.Size(78, 20);
+            this.lblRelatorioEndereco.TabIndex = 3;
+            this.lblRelatorioEndereco.Text = "Endereço";
+            // 
+            // lblRelatorioEmail
+            // 
+            this.lblRelatorioEmail.AutoSize = true;
+            this.lblRelatorioEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRelatorioEmail.ForeColor = System.Drawing.Color.Black;
+            this.lblRelatorioEmail.Location = new System.Drawing.Point(27, 143);
+            this.lblRelatorioEmail.Name = "lblRelatorioEmail";
+            this.lblRelatorioEmail.Size = new System.Drawing.Size(48, 20);
+            this.lblRelatorioEmail.TabIndex = 4;
+            this.lblRelatorioEmail.Text = "Email";
+            // 
+            // lblRelatorioTelefone
+            // 
+            this.lblRelatorioTelefone.AutoSize = true;
+            this.lblRelatorioTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRelatorioTelefone.ForeColor = System.Drawing.Color.Black;
+            this.lblRelatorioTelefone.Location = new System.Drawing.Point(25, 187);
+            this.lblRelatorioTelefone.Name = "lblRelatorioTelefone";
+            this.lblRelatorioTelefone.Size = new System.Drawing.Size(71, 20);
+            this.lblRelatorioTelefone.TabIndex = 5;
+            this.lblRelatorioTelefone.Text = "Telefone";
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.BackColor = System.Drawing.Color.Black;
+            this.btnConfirmar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirmar.ForeColor = System.Drawing.Color.White;
+            this.btnConfirmar.Location = new System.Drawing.Point(143, 211);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(94, 37);
+            this.btnConfirmar.TabIndex = 6;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = false;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(512, 373);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnGerarRelatorio);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtEndereço);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblCadastro);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblEndereco);
+            this.Controls.Add(this.lblIdade);
+            this.Controls.Add(this.lblNome);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Relatório";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblIdade;
+        private System.Windows.Forms.Label lblEndereco;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.Label lblCadastro;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.TextBox txtEndereço;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Button btnGerarRelatorio;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Label lblRelatorioTelefone;
+        private System.Windows.Forms.Label lblRelatorioEmail;
+        private System.Windows.Forms.Label lblRelatorioEndereco;
+        private System.Windows.Forms.Label lblRelatorioIdade;
+        private System.Windows.Forms.Label lblRelatorioNome;
+    }
+}
+
---
+++ Aula1009/Aula1009/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula1009
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void botApresentar(object sender, EventArgs e)
+        {
+            string nome = txtNome.Text;
+            //int idade = Int32.Parse(txtIdade.Text);
+            string idade = txtIdade.Text;
+            string endereco = txtEndereço.Text;
+            string email = txtEmail.Text;
+            string telefone = txtTelefone.Text;
+
+            if (string.IsNullOrEmpty(nome) || string.IsNullOrEmpty(idade) || string.IsNullOrEmpty(endereco) || string.IsNullOrEmpty(email) || string.IsNullOrEmpty(telefone))
+            {
+                MessageBox.Show("Preencha todos os campos!");
+            } else
+            {
+                panel1.Visible = true;
+                lblRelatorioNome.Text = $"Nome: {nome}";
+                lblRelatorioIdade.Text = $"Idade: {idade}";
+                lblRelatorioEndereco.Text = $"Endereço: {endereco}";
+                lblRelatorioEmail.Text = $"Email: {email}";
+                lblRelatorioTelefone.Text = $"Telefone: {telefone}";
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            txtNome.Text = "";
+            txtIdade.Text = "";
+            txtEndereço.Text = "";
+            txtEmail.Text = "";
+            txtTelefone.Text = "";
+        }
+
+        private void ValidarIdade(object sender, KeyPressEventArgs e)
+        {
+            char key = e.KeyChar;
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+    }
+}
